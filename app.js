905192c6b6e8dbf19f906a132bd44d4f593ff045
SiamIslamSagor <msiamislam12345@gmail.com>
2024-09-11 00:02:22 +0600

    try to fix deployment issue

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const express = require("express");
+import express from "express";
 
 const app = express();
-const PORT = 8000;
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("Hello World");
@@ -12,5 +12,5 @@ app.get("/about", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`✅ Server is running on port ${PORT}`);
+  console.log(`SketchStream Server is running on port ${PORT}`);
 });
