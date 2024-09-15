cfd1b6ce2fd1359bcff00a206eb5ba0dcd5e138e
SiamIslamSagor <msiamislam12345@gmail.com>
2024-09-16 01:06:26 +0600

    added cors origin

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 import { errorMiddleware } from "./middlewares/error.js";
@@ -17,6 +18,15 @@ connectDB(mongoURI);
 
 const app = express();
 
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://sketch-stream-client.vercel.app",
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
